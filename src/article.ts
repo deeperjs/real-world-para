fa4483d4aab403ac6941ac2a158f458044064083
Mateusz <mateusz.kwasniewski@bottega.com.pl>
2025-03-13 12:57:03 +0100

    article repository

---
+++ src/article.ts
@@ -0,0 +1,21 @@
+// tiny types
+
+export type Tag = string;
+export type ArticleId = string;
+export type Slug = string;
+// tiny types
+// type Slug = `${string}_slug`;
+// type Slug = string & {_kind: 'slug'};
+// const Slug = (slug: string) => slug as Slug;
+// const x: Slug = Slug("abc");
+
+export type Article = {
+    body: string;
+    description: string;
+    tagList: Array<string>;
+    title: string;
+    slug: Slug;
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
