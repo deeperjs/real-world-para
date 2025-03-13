e4bd51671ed80d19c9ab8b9242aad6db7fccb164
Mateusz <mateusz.kwasniewski@bottega.com.pl>
2025-03-13 12:36:08 +0100

    error handler

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,12 @@
-import express, {ErrorRequestHandler} from "express";
+import express from "express";
 import cors from "cors";
-import {NotFoundError} from "./NotFoundError";
 import {articlesRouter} from "./articlesRouter";
+import {errorHandler, notFoundHandler} from "./errorHandler";
 
 export const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(articlesRouter);
 
-app.use((req, res, next) => {
-  throw new NotFoundError();
-});
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  if (err instanceof NotFoundError) {
-    res.status(404).json({ errors: err.message });
-    return;
-  }
-  console.error(err);
-  res.sendStatus(500);
-};
+app.use(notFoundHandler);
 app.use(errorHandler);
