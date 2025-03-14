512b9b6f26f31ce7a7fa11da5afc0b78dd96e9d3
Mateusz <mateusz.kwasniewski@bottega.com>
2025-03-14 13:28:05 +0100

    injecting config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import { app } from "./app";
+import {createApp} from "./app";
 import { config } from "./config";
 
 const port = config.PORT;
+const app = createApp(config);
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
